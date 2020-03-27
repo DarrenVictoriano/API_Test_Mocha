a15e982af33f2cadc20176daf14e2f0e64967a67
Darren Victoriano <darren.victoriano@gmail.com>
2020-03-26 19:55:12 -0700

    added getCurrentTime

---
+++ test/config/config.js
@@ -0,0 +1,20 @@
+const IP_ADDRESS = "192.168.0.15";
+const AUTH_CODE = "darren";
+
+module.exports = {
+    getBaseURI: function () {
+        return "http://" + IP_ADDRESS + "/sony";
+    },
+    getAuthCode: function () {
+        return AUTH_CODE;
+    },
+    getHeaders: function () {
+        axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-AUth-PSK': AUTH_CODE
+            }
+        }
+        return axiosConfig;
+    }
+}
---
+++ test/getCurrentTime.js
@@ -0,0 +1,46 @@
+const envConfig = require('./config/config');
+const axios = require('axios');
+const expect = require('chai').expect;
+
+describe('getCurrentTime', function () {
+    context('correct parameters', function () {
+        it('should return current time', function () {
+
+            body = {
+                "method": "getCurrentTime",
+                "id": 51,
+                "params": [],
+                "version": "1.0"
+            }
+
+            return axios.post(envConfig.getBaseURI() + "/system", body, envConfig.getHeaders)
+                .then(res => {
+                    //console.log(res.data);
+
+                    actualResponse = res.data;
+                    expect(actualResponse.error).to.be.undefined;
+                    expect(actualResponse.result).to.not.be.null;
+                });
+        });
+    });
+    context('incorrect parameters', function () {
+        it('should still return the current time', function () {
+
+            body = {
+                "method": "getCurrentTime",
+                "id": 51,
+                "params": [11],
+                "version": "1.0"
+            }
+
+            return axios.post(envConfig.getBaseURI() + "/system", body, envConfig.getHeaders)
+                .then(res => {
+                    //console.log(res.data);
+
+                    actualResponse = res.data;
+                    expect(actualResponse.result).to.not.be.null;
+                    expect(actualResponse.error).to.be.undefined;
+                });
+        });
+    });
+});
